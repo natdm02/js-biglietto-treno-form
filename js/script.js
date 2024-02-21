6d4d481d527681bca76b5830602214a760fef87a
natdm02 <natalidimarino2002>
2024-02-21 16:48:06 +0100

    [add]calcolo costo biglietto/numeri random

--- js/script.js
+++ js/script.js
@@ -14,6 +14,35 @@ function() {
 
  // var km    
     var inputKm = document.getElementById("km");
-    
 
+    var percorrenza = parseInt(inputKm.value);
+
+    var prezzoAlKilometro = 0.21;
+    var prezzoBiglietto = prezzoAlKilometro * percorrenza;
+    var offerta = "Tariffa Standard";
+
+ // calcolo costo biglietto con sconti del 20 e 40% 
+
+    if (inputEta == "minorenne") {
+        prezzoBiglietto -= (prezzoBiglietto * 20 / 100); 
+        offerta = "Sconto Minorenne";
+    } else if (inputEta == "over65") {
+        prezzoBiglietto -= (prezzoBiglietto * 40 / 100);
+        offerta = "Sconto Maggiorenne";   
+    } 
+    
+ //numero random per carrozza 
+    var carrozza = Math.floor(Math.random() * 9) +1; 
+    var cp = Math.floor(Math.random() * (100000 - 90000 + 1 )) + 90000;  
+    
+ //stampiamo
+    document.getElementById('nome-passeggero').innerHTML = inputNome;
+    document.getElementById('offerta').innerHTML = offerta;
+    document.getElementById('costo-biglietto').innerHTML = prezzoBiglietto.toFixed(2) + " Euro" 
+    document.getElementById('carrozza').innerHTML = carrozza;
+    document.getElementById('codice-cp').innerHTML = cp;
+
+    var bigliettoStampato = document.getElementById('biglietto');
+    bigliettoStampato.classList.remove('hidden');
+    bigliettoStampato.classList.add('show');
 });
