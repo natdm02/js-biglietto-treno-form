c0fb96109f602940b96e338cb906f41f19b6b410
natdm02 <natalidimarino2002>
2024-02-21 15:57:33 +0100

    [add]variabili nome età e km

---
+++ js/script.js
@@ -0,0 +1,19 @@
+ // variabile BUTTON 'GENERA'
+var buttonGenera = document.getElementById('genera');
+
+
+ //click BUTTON 'genera'
+buttonGenera.addEventListener( 'click',
+function() {
+
+ // var nome dell'utente 
+    var inputNome = document.getElementById("nome-utente").value;
+    
+ // var età dell'utente 
+    var inputEta = document.getElementById("fascia-eta").value;
+
+ // var km    
+    var inputKm = document.getElementById("km");
+    
+
+});
